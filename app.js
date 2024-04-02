53765d44bcedbb20392b42e8d68fd436930d9a50
YYanti00 <114889196+YYanti00@users.noreply.github.com>
2024-04-02 12:11:56 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Hello world!\n')
+    res.send(`Nama: Linda Yanti Yo\n NIM: 52022902\n Jurusan: Teknik Informatika`)
 })
 
 app.listen(PORT, HOST)
